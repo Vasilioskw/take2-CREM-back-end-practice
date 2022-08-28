f2083ff4ae256b74bf414d0b6fbce6adcb533a0e
Vasilios Kambouras <Vasilioskw@gmail.com>
2022-08-28 14:09:02 -0400

    fixed issue from original practice proj, tsc was not properly installed

---
+++ take2-CREM-back-end-practice/server/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
+// import * as "typeorm";
+import { DataSource } from "typeorm";
+
+
+
+const main = async () => {  
+    const app = express() // Creates an instance of express and allows middleware (including graphql)
+    app.use(cors()) // Middleware fro back to front end
+    app.use(express.json()) // This just means, we will be using json as the body for every request to server (Which makes it easy for graphql)
+   
+   
+    // app.use("/graphql", graphqlHTTP({
+    //     schema,
+    //     graphql: true
+
+    // }))
+    // We will uncomment when this is defined
+
+
+    console.log("index.ts is being read!")
+
+    app.listen(3001, () => {
+        console.log("SERVER RUNNING ON PORT 3001")
+    });
+};
+
+main().catch((err) => {
+    console.log(err);
+}); 
+
